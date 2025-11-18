cffa4906c355ad0be30f324d4ab1d5a99904e70f
TANISHRANJAN <130069269+TANISHRANJAN@users.noreply.github.com>
2025-11-18 11:55:18 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is the About page.</p>
+      <p>Testing this .</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+
+        {/* Define your routes here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
